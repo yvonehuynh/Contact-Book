01cb4bbc750350b376cd93a0bd4edcdb9d638cd4
yvonehuynh <31800143+yvonehuynh@users.noreply.github.com>
2017-12-26 20:54:47 -0500

    added inputs and states

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      contacts: {},
+      name: "",
+      address: ""
+    };
+    this.onChange = this.onChange.bind(this)
+  };
+  onChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
     render() {
       return (
         <div>
-          Hello
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.onChange}/>
+            <label htmlFor="address">Address</label>
+            <input type="text" name="address" value={this.state.address} onChange={this.onChange}/>
+          </form>
         </div>
       )
     }
