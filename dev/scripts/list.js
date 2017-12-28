62cdaed2a76f5b35ae4683892f6c435e3a3531b8
yvonehuynh <31800143+yvonehuynh@users.noreply.github.com>
2017-12-28 11:34:18 -0500

    added editing functionality

--- dev/scripts/list.js
+++ dev/scripts/list.js
@@ -3,16 +3,54 @@ import React from "react";
 export default class List extends React.Component {
     constructor(){
         super();
+        this.state={
+            editing: false,
+            contactInfo: {}
+        }
+        this.save = this.save.bind(this)
+    }
+    save(e) {
+        e.preventDefault();
+        const dbRef = firebase.database().ref(this.props.data.key);
+        dbRef.update({
+            name: this.name.value,
+            address: this.address.value,
+            work: this.work.value,
+            other: this.other.value
+        });
+
+        this.setState({
+            editing: false
+        });
     }
     render(){
- 
+        let editingTemp = (
+            <span>
+                <p>{this.props.data.name}</p>
+                <p>Home - {this.props.data.address}</p>
+                <p>Work - {this.props.data.work}</p>
+                <p>Other - {this.props.data.other}</p>
+            </span>
+        )
+        if (this.state.editing) {
+            editingTemp = (
+                <form onSubmit={this.save}>
+                    <input type="text" name="name" defaultValue={this.props.data.name} onChange={this.onChange} ref={ref => this.name = ref} />
+                   
+                    <input type="text" name="address" id="address" defaultValue={this.props.data.address} onChange={this.onChange} ref={ref => this.address = ref} />
+                    <input type="text" name="work" id="work" defaultValue={this.props.data.work} onChange={this.onChange} ref={ref => this.work = ref} />
+                    <input type="text" name="other" id="other" defaultValue={this.props.data.other} onChange={this.onChange} ref={ref => this.other = ref} />
+                    <input type="submit" value="Done editing" />
+                </form>
+            )
+        }
         return (
             
             <li key={this.props.data.key}>
-                    {this.props.data.name}
-                    Home - {this.props.data.address}
-                    Work - {this.props.data.work}
-                    Other - {this.props.data.other}
+                    {editingTemp}
+                <button onClick={() => {
+                    this.setState({ editing: true })
+                }}>edit</button>
                     <button onClick={()=>this.props.remove(this.props.data.key)}>Remove</button>
                 </li>
             
